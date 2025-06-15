445ac62b8fb2707d5491ef326d865786c78adb6a
Leticia Takahashi <leticiasatiko@gmail.com>
2025-06-15 15:54:42 -0300

    filtrar endpoints

---
+++ src/main/java/com/projetoAuth/jwt/config/SecurityFilter.java
@@ -0,0 +1,49 @@
+package com.projetoAuth.jwt.config;
+
+import com.projetoAuth.jwt.infra.TokenService;
+import com.projetoAuth.jwt.repository.userRepository;
+import com.projetoAuth.jwt.model.userModel;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private userRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+
+        if (token != null) {
+            var login = tokenService.validateToken(token);
+            if (login != null) {
+                userModel user = userRepository.findByLogin(login);
+                if (user != null) {
+                    var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+}
