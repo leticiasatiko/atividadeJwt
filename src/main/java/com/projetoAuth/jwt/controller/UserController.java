ee01113ee5f01591029a295d7bfc754ebeb5b8f8
Leticia Takahashi <leticiasatiko@gmail.com>
2025-06-15 16:27:29 -0300

    list all apenas para admin

--- src/main/java/com/projetoAuth/jwt/controller/UserController.java
+++ src/main/java/com/projetoAuth/jwt/controller/UserController.java
@@ -1,14 +1,23 @@
 package com.projetoAuth.jwt.controller;
 
+import com.projetoAuth.jwt.model.userModel;
+import com.projetoAuth.jwt.repository.userRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize; // Importe para usar @PreAuthorize
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private userRepository userRepository;
+
     @GetMapping("/public")
     public ResponseEntity<String> publicEndpoint() {
         return ResponseEntity.ok("Este é um endpoint público, acessível a todos.");
@@ -24,5 +33,12 @@ public class UserController {
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<String> userSpecificEndpoint() {
         return ResponseEntity.ok("Bem-vindo, USUÁRIO! Você tem acesso às funcionalidades de usuário.");
+    }
+
+    @GetMapping("/list-all")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<userModel>> listAllUsers() {
+        List<userModel> users = userRepository.findAll();
+        return ResponseEntity.ok(users);
     }
 }
